ac789686797e1f0952403053dc70bc7b8c0efd19
Bruno Barbosa <brunnobarbosas@gmail.com>
2022-05-09 23:26:00 -0300

    estilos navbar

--- frontend/src/Navbar/index.tsx
+++ frontend/src/Navbar/index.tsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import {ReactComponent as GithubIcon} from '../assets/img/github-1.svg'
-
+import './styles.css'
 
 export default function index() {
   return (
     <header>
-      <nav>
-        <div>
+      <nav className="container">
+        <div className="dsmovie-nav-content">
           <h1>DSMovie</h1>
 
           <a href="https://github.com/brunodelucasbarbosa">
-            <div>
+            <div className="dsmovie-contact-container">
               <GithubIcon />
-              <p>brunodelucasbarbosa</p>
+              <p className="ds-movie-contact-link">brunodelucasbarbosa</p>
             </div>
           </a>
         </div>
