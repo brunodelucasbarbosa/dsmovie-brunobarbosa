ab9933e1539ccc654c88f1694e263d535ad89f90
Bruno Barbosa <brunnobarbosas@gmail.com>
2022-05-10 00:22:43 -0300

    fix margin

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -8,21 +8,20 @@ export default function index() {
       <Pagination />
       <div className="container">
         <div className="row">
-          <div className="col-sm-6 col-lg-4 col-xl-3">
+          
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
             <MovieCard />
           </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3">
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
             <MovieCard />
           </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3">
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
             <MovieCard />
           </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3">
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
             <MovieCard />
           </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3">
-            <MovieCard />
-          </div>
+
         </div>
       </div>
     </>
