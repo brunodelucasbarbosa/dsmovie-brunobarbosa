37986ea7bdf423e1af270349e8c582d7469fc32a
Bruno Barbosa <brunnobarbosas@gmail.com>
2022-05-13 11:52:36 -0300

    mudancas

--- backend/src/main/java/com/devsuperior/dsmovie/controllers/ScoreController.java
+++ backend/src/main/java/com/devsuperior/dsmovie/controllers/ScoreController.java
@@ -2,10 +2,6 @@ package com.devsuperior.dsmovie.controllers;
 
 import com.devsuperior.dsmovie.dto.MovieDTO;
 import com.devsuperior.dsmovie.dto.ScoreDTO;
-import com.devsuperior.dsmovie.services.MovieService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
