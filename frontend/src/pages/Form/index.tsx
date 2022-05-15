aadba1ef2226837d8c6f0b1d044b3ba3a6245d35
Bruno Barbosa <brunnobarbosas@gmail.com>
2022-05-15 04:03:23 -0300

    stars

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -2,7 +2,7 @@ import FormCard from 'components/FormCard';
 import { useParams } from 'react-router-dom';
 
 
-export default function index() {
+export default function Form() {
 
     const params = useParams();
 
